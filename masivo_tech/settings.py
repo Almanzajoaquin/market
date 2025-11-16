dd38ac2148f28696016cf8f16d76c731157bfa7c
Tomas Fernandez <tomas.jobs.gr@gmail.com>
2025-11-16 15:36:39 -0300

    Actualizacion de estructura settings, para conexion postgre

--- masivo_tech/settings.py
+++ masivo_tech/settings.py
@@ -1,5 +1,4 @@
 # === settings.py - Configuración Django Optimizada ===
-
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -109,22 +108,34 @@ WSGI_APPLICATION = 'masivo_tech.wsgi.application'
 # CONFIGURACIÓN DE BASE DE DATOS
 # =============================================================================
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-    #'default': dj_database_url.config(
-    #    default=f"sqlite:///{BASE_DIR / 'db.slite3'}",
-    #    conn_max_age=600
-    #)
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL'),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+#     #'default': dj_database_url.config(
+#     #    default=f"sqlite:///{BASE_DIR / 'db.slite3'}",
+#     #    conn_max_age=600
+#     #)
     
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     #}
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+
 
 # =============================================================================
 # CONFIGURACIÓN DE AUTENTICACIÓN
