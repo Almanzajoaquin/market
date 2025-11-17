9a84b44cd1b6d15f968e60b3355ea573e2b4308b
Tomas Fernandez <tomas.jobs.gr@gmail.com>
2025-11-16 23:23:17 -0300

    SH final si no anda con esto....

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-# Render build script
-
 echo "Installing dependencies..."
 pip install -r requirements.txt
+
+# DEBUG CLOUDINARY
+echo "=== CLOUDINARY DEBUG ==="
+python -c "
+import os
+print('CLOUD_NAME:', os.getenv('CLOUDINARY_CLOUD_NAME'))
+print('API_KEY:', os.getenv('CLOUDINARY_API_KEY')[:10] + '...' if os.getenv('CLOUDINARY_API_KEY') else 'None')
+print('API_SECRET:', os.getenv('CLOUDINARY_API_SECRET')[:10] + '...' if os.getenv('CLOUDINARY_API_SECRET') else 'None')
+"
 
 echo "Make Migrations..."
 python manage.py makemigrations
